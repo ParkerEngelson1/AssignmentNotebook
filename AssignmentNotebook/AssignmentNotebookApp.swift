e39b5a67007a195bb22de788d9e65e3d5a49d61a
Parker <Parker.engelson09@gmail.com>
2026-01-30 10:17:25 -0800

    Initial Commit

---
+++ AssignmentNotebook/AssignmentNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentNotebookApp.swift
+//  AssignmentNotebook
+//
+//  Created by Parker Engelson on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentNotebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AssignmentNotebook
+//
+//  Created by Parker Engelson on 1/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
